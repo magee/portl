5588aada88ece427a32fced621b17789d86a7568
Magee Mooney <mageemooney@me.com>
2014-05-06 22:28:38 -0700

    modifies update code to reflect our product data model

--- collections/products.js
+++ collections/products.js
@@ -1,7 +1,10 @@
 /**
  * Created by mageemooney on 5/4/14.
  */
-Products = new Meteor.Collection('products');
+//TODO: don't pull data over the wire if people are just visiting static content
+if (user) {
+  Products = new Meteor.Collection('products');
+}
 
 //TODO add second allow callback to check if user is admin to allow edits (pg 137)
 Products.allow({
@@ -9,19 +12,20 @@ Products.allow({
   remove: ownsDocument
 });
 
-// Rejects updates when any attributes are submitted other than url and title
-Products.deny({
-  update: function (userID, product, fieldNames) {
-    return (_.without(fieldNames, 'url', 'title').length > 0);
-  }
-});
+// Rejects updates when any attributes are submitted other than sku and title
+//Products.deny({
+//  update: function (userID, product, fieldNames) {
+//    return (_.without(fieldNames, 'sku', 'title').length > 0);
+//  }
+//});
 
 Meteor.methods({
   post: function(productAttributes) {
     var user = Meteor.user(),
-        productWithSameLink = Products.findOne({ url: productAttributes.url });
+        productWithSameFamily = Products.findOne({ family: productAttributes.family });
 
     if (!user) {
+      //TODO Find out where these errors are caught.
       throw new Meteor.Error(401, 'You must be logged in to create new products.');
     }
 
@@ -29,12 +33,11 @@ Meteor.methods({
       throw new Meteor.Error(422, 'A Product title is required');
     }
 
-    if (productAttributes.url && productWithSameLink) {
-      throw new Meteor.Error(302, 'This product has already been created', productWithSameLink._id);
+    if (productAttributes.family && productWithSameFamily) {
+      throw new Meteor.Error(302, 'This product has already been created', productWithSameFamily._id);
     }
 
-    var product = _.extend(_.pick(productAttributes, 'url', 'title', 'description'), {
-      title     : productAttributes.title,
+    var product = _.extend(productAttributes, {
       userId    : user._id,
       author    : user.username,
       submitted : new Date().getTime()
