c313b5b09e15a3490f8e00721512791b6e596015
Magee Mooney <mageemooney@me.com>
2014-05-10 23:32:47 -0700

    Adds limit and skip to Product cursor to allow for pagination

--- server/services.js
+++ server/services.js
@@ -1,34 +1,36 @@
 /**
  * Created by mageemooney on 5/5/14.
  */
-// first, remove configuration entry in case service is already configured
-/*
-Accounts.loginServiceConfiguration.remove({
-  service: "facebook"
-});
-Accounts.loginServiceConfiguration.insert({
-  service: "facebook",
-  appId: "yourAppId",
-  secret: "yourSecret"
-});
+Meteor.methods({
+  getProductCount: function() {
+    return Products.find().count();
+  },
+  post: function(productAttributes) {
+    var user = Meteor.user(),
+        //TODO this doesn't work with new paginated cursor.  Revise.
+        productWithSameFamily = Products.findOne({ family: productAttributes.family });
 
-// first, remove configuration entry in case service is already configured
-Accounts.loginServiceConfiguration.remove({
-  service: "twitter"
-});
-Accounts.loginServiceConfiguration.insert({
-  service: "twitter",
-  consumerKey: "yourConsumerKey",
-  secret: "yourSecret"
-});
+    if (!user) {
+      //TODO Find out where these errors are caught.
+      throw new Meteor.Error(401, 'You must be logged in to create new products.');
+    }
 
-*/
-// first, remove configuration entry in case service is already configured
-//Accounts.loginServiceConfiguration.remove({
-//  service: "google"
-//});
-//Accounts.loginServiceConfiguration.insert({
-//  service: "google",
-//  clientId: "616940920145-b8edlpjdphvdj89qehqjsot9rclrvgar.apps.googleusercontent.com",
-//  secret: "8L5Ga1q5O6FX5pq7URYUChQS"
-//});
+    if (!productAttributes.title) {
+      throw new Meteor.Error(422, 'A Product title is required');
+    }
+
+    if (productAttributes.family && productWithSameFamily) {
+      throw new Meteor.Error(302, 'This product has already been created', productWithSameFamily._id);
+    }
+
+    var product = _.extend(productAttributes, {
+      userId    : user._id,
+      author    : user.username,
+      createdAt : new Date().getTime()
+    });
+
+    var productID = Products.insert(product);
+
+    return productID;
+  }
+});
