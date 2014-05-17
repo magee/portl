9edf509abad280e2dabfdcd3f00988ad48007cfd
Magee Mooney <mageemooney@me.com>
2014-05-17 15:14:22 -0700

    modifies new variant form layout on products page.  Adds Helper to pull color names (for typeahead -- not implemented yet)

--- client/views/variants/variant_submit.js
+++ client/views/variants/variant_submit.js
@@ -1,40 +1,51 @@
 /**
  * Created by mageemooney on 5/16/14.
  */
+Meteor.autorun(function(){
+  Meteor.subscribe('colors');
+})
+
 Template.variantSubmit.events({
   'submit form': function(e, template) {
     e.preventDefault();
-    var $sku    = $(e.target).find('[name=sku]');
-//    var $title  = $(e.target).find('[name=title]');
     var $color  = $(e.target).find('[name=color]');
     var $size   = $(e.target).find('[name=size]');
+    var $qty    = $(e.target).find('[name=qty]');
     var $cost   = $(e.target).find('[name=cost]');
     var $price  = $(e.target).find('[name=price]');
 
+    var code = Colors.find({name: $color.val()}, {colorCode: 1});
+    console.log(code);
+
     var variant = {
-      sku       : $sku.val(),
-//      title     : $title.val(),
+      sku       : template.data.family + '-' + $color.val() + '-' + $size.val(),
       title     : $color.val() + ' / ' + $size.val(),
       color     : $color.val(),
       size      : $size.val(),
+      qty       : $qty.val(),
       cost      : $cost.val(),
       price     : $price.val(),
       productId : template.data._id
     };
 
-    //TODO:  productID above pulls the variant ID.  Need to pass the productID to the template
+    console.log(variant);
 
     Meteor.call('variant', variant, function(error, variantId) {
       if (error){
         throwError(error.reason);
       } else {
-        $sku.val('');
-//        $title.val('');
         $color.val('');
+        $qty.val('');
         $size.val('');
-        $cost.val('');
-        $price.val('');
+//        $cost.val('');
+//        $price.val('');
       }
     });
+  }
+});
+
+Template.variantSubmit.helpers({
+  colorList: function() {
+    return Colors.find().fetch().map(function(it){ return it.name; });
   }
 });
