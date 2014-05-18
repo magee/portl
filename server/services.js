a703cb23bb8451aae5d48c9c1a6b7b9723983c1d
Magee Mooney <mageemooney@me.com>
2014-05-17 21:36:21 -0700

    remove collection-specific code from server/services.js and added abstracted some collection handling

--- server/services.js
+++ server/services.js
@@ -1,36 +1,14 @@
 /**
  * Created by mageemooney on 5/5/14.
  */
+
 Meteor.methods({
-  getProductCount: function() {
-    return Products.find().count();
+  findLast : function(collection, filter){
+    var criteria = filter || '{}';
+    return collection.findOne(criteria, {sort: {createdAt: -1}});
   },
-  post: function(productAttributes) {
-    var user = Meteor.user(),
-        //TODO this doesn't work with new paginated cursor.  Revise.
-        productWithSameFamily = Products.findOne({ family: productAttributes.family });
-
-    if (!user) {
-      //TODO Find out where these errors are caught.
-      throw new Meteor.Error(401, 'You must be logged in to create new products.');
-    }
-
-    if (!productAttributes.title) {
-      throw new Meteor.Error(422, 'A Product title is required');
-    }
-
-    if (productAttributes.family && productWithSameFamily) {
-      throw new Meteor.Error(302, 'This product has already been created', productWithSameFamily._id);
-    }
-
-    var product = _.extend(productAttributes, {
-      userId    : user._id,
-      author    : user.username,
-      createdAt : new Date().getTime()
-    });
-
-    var productID = Products.insert(product);
-
-    return productID;
+  getCount : function(collection, filter) {
+    return collection.find(filter).count();
   }
 });
+
