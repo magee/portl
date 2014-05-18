deda355479c5a56a3cc36cb900d25dc85904552e
Magee Mooney <mageemooney@me.com>
2014-05-17 21:38:38 -0700

    adds product CRUD methods to product.js collection file

--- collections/products.js
+++ collections/products.js
@@ -9,23 +9,79 @@
  * Example:  Product:  Striped seersucker suit
  *           Variants:   blue/6, blue/8, blue/10, cream/6, cream/12, etc.
  */
-////TODO: don't pull data over the wire if people are just visiting static content
-//if (this.userId) {
-//  Products = new Meteor.Collection('products');
-//
-//  //TODO add second allow callback to check if user is admin to allow edits (pg 137)
-//  Products.allow({
-//    update: ownsDocument,
-//    remove: ownsDocument
-//  });
-//}
 
-//TODO: restore above code after seeding modulus.io database with data from fixtures.js
-  Products = new Meteor.Collection('products');
+Products = new Meteor.Collection('products');
 
-  //TODO add second allow callback to check if user is admin to allow edits (pg 137)
-  Products.allow({
-//    update: ownsDocument,
-//    remove: ownsDocument
+//TODO revise code to check if user is admin to allow edits (pg 137)
+Products.allow({
+  update: ownsDocument,
+  remove: ownsDocument
+});
+
+// TODO: get the next productNo - must be run on server where it has access to entire collection
+//  getNextProductNumber: function(vendor) {
+//    var vendorProducts = Products.find({vendor: vendor}, {family: 1});
+//    console.log(vendorProducts)
+//    return 5;
+//  },
+//  getProductCount: function() {
+//    return Products.find().count();
+//  }
+
+if (Meteor.isServer) {
+  Meteor.methods({
+    addProduct: function(productAttributes) {
+      var user = Meteor.user(),
+          //TODO this doesn't work with new paginated cursor.  Revise.
+          productWithSameFamily = Products.findOne({ family: productAttributes.family });
+
+      if (!user) {
+        //TODO Find out where these errors are caught.
+        throw new Meteor.Error(401, 'You must be logged in to create new products.');
+      }
+
+      if (!productAttributes.title) {
+        throw new Meteor.Error(422, 'A Product title is required');
+      }
+
+      if (productAttributes.family && productWithSameFamily) {
+        throw new Meteor.Error(302, 'This product has already been created', productWithSameFamily._id);
+      }
+
+      var product = _.extend(productAttributes, {
+        userId    : user._id,
+        author    : user.username,
+        createdAt : new Date().getTime()
+      });
+
+      var productID = Products.insert(product);
+
+      return productID;
+    },
+    updateProduct : function(productAttributes) {
+      var currentProductId = productAttributes.productId;
+
+      Products.update(currentProductId, {$set: _.omit(productAttributes, 'productId')}, function (error) {
+        if (error) {
+          throwError(error.reason);
+        } else {
+          Router.go('productPage', {_id: currentProductId});
+        }
+      });
+    },
+    deleteProduct : function(productId) {
+      Products.remove(productId, function(error) {
+        if (error) {
+          throwError(error.reason);
+          //TODO: does this next line execute if an error is thrown?
+          Router.go('productPage', {_id: productId});
+        } else {
+          Router.go('productsList');
+        }
+      });
+    }
   });
+}
+
+
 
