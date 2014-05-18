d877276c77de7642f38c3f350ce7c2166f0536e8
Magee Mooney <mageemooney@me.com>
2014-05-17 23:28:44 -0700

    renames addVariant method name.  Removes unnecessary redundant variables.

--- client/views/variants/variant_submit.js
+++ client/views/variants/variant_submit.js
@@ -8,32 +8,27 @@ Meteor.autorun(function(){
 Template.variantSubmit.events({
   'submit form': function(e, template) {
     e.preventDefault();
+
     var $color  = $(e.target).find('[name=color]');
     var $size   = $(e.target).find('[name=size]');
-    var $qty    = $(e.target).find('[name=qty]');
-    var $cost   = $(e.target).find('[name=cost]');
-    var $price  = $(e.target).find('[name=price]');
-
     var code = Colors.find({name: $color.val()}, {colorCode: 1});
-    console.log(code);
 
     var variant = {
       sku       : template.data.family + '-' + $color.val() + '-' + $size.val(),
       title     : $color.val() + ' / ' + $size.val(),
       color     : $color.val(),
       size      : $size.val(),
-      qty       : $qty.val(),
-      cost      : $cost.val(),
-      price     : $price.val(),
+      qty       : $(e.target).find('[name=qty]').val(),
+      cost      : $(e.target).find('[name=cost]').val(),
+      price     : $(e.target).find('[name=price]').val(),
       productId : template.data._id
     };
 
-    console.log(variant);
-
-    Meteor.call('variant', variant, function(error, variantId) {
+    Meteor.call('addVariant', variant, function(error, variantId) {
       if (error){
         throwError(error.reason);
       } else {
+//        disabled to speed up multiple variant creation.  Uncomment as necessary during user testing.
 //        $color.val('');
 //        $qty.val('');
 //        $size.val('');
