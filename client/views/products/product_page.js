84a01babd1c027397157ec8f4847f6d3568094fb
Magee Mooney <mageemooney@me.com>
2014-05-17 23:32:07 -0700

    adds delete click event handler to product details page

--- client/views/products/product_page.js
+++ client/views/products/product_page.js
@@ -2,6 +2,31 @@
  * Created by mageemooney on 5/8/14.
  */
 
+Template.productPage.events({
+  'click .delete': function(e) {
+    e.preventDefault();
+
+    if (confirm('This will delete the product and all its variants.  Continue?')) {
+
+      Meteor.call('deleteProduct', this._id, function(error, id) {
+
+        if (error) {
+          throwError(error.reason);
+
+          if (error.error === 302) {
+            Router.go('productPage', {_id: error.details});
+          }
+        } else {
+          Router.go('productsList');
+        }
+      });
+
+      Router.go('productsList');
+    }
+
+  }
+});
+
 Template.productPage.helpers({
    variants:  function() {
      return Variants.find({productId: this._id});
