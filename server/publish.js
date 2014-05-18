4bcf4bdb401d3926f2a79695f26ed8111bf9f235
Magee Mooney <mageemooney@me.com>
2014-05-17 23:22:35 -0700

    corrects bad Product cursor publish statement

--- server/publish.js
+++ server/publish.js
@@ -7,7 +7,8 @@ Meteor.publish('products', function(productCursor){
   return Products.find({},{
     skip: productCursor,
     limit: 4,
-    title: {_id: 1}});
+    sort: {vendor: 1, sku: 1}
+  });
 //  return Products.find({}, {limit: 5, skip: productPage});
 });
 Meteor.publish('product', function(productID){
