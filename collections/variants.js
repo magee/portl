ac4be75890c9e978310649bb9dc1a268dac99d32
Magee Mooney <mageemooney@me.com>
2014-05-17 21:39:47 -0700

    adds variant CRUD methods to variant.js collection file

--- collections/variants.js
+++ collections/variants.js
@@ -8,10 +8,11 @@
  */
 
 Variants = new Meteor.Collection('variants');
+var user = Meteor.user();
 
 Meteor.methods({
-  variant: function(variantAttributes) {
-    var user = Meteor.user();
+  addVariant: function(variantAttributes) {
+//    var user = Meteor.user();
     var product = Products.findOne(variantAttributes.productId);
 
 // ensure the user is logged in
@@ -29,5 +30,31 @@ Meteor.methods({
     });
 
     return Variants.insert(variant);
+  },
+  updateVariant: function(variantAttributes) {
+//    var user = Meteor.user();
+    var product = Products.findOne(variantAttributes.productId);
+
+// ensure the user is logged in
+    if (!user)
+      throw new Meteor.Error(401, "You need to login to create variants");
+    if (!variantAttributes.title)
+      throw new Meteor.Error(422, 'Please add variant data');
+    if (!product)
+      throw new Meteor.Error(422, 'A variant must be related to a product');
+
+    variant = _.extend(variantAttributes, {
+      userId: user._id,
+      author: user.username,
+      submitted: new Date().getTime()
+    });
+
+    return Variants.insert(variant);
+  },
+  deleteVariant: function(variantID) {
+    Variants.remove(variantID);
   }
 });
+
+
+
