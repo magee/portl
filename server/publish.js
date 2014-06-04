eaf1ecac2497a9577d98ddb256e9dc62a4c261f4
Magee Mooney <mageemooney@me.com>
2014-06-04 06:11:34 -0700

    style changes to publish.js to pass jshint

--- server/publish.js
+++ server/publish.js
@@ -11,28 +11,36 @@ Meteor.publish('products', function(productCursor){
   });
 //  return Products.find({}, {limit: 5, skip: productPage});
 });
-Meteor.publish('product', function(productID){
+Meteor.publish('product', function (productID){
   return Products.find(productID);
 });
 
-Meteor.publish('variants', function(productID){
-  return Variants.find({productId: productID});
+Meteor.publish('variants', function (productId) {
+  return Variants.find({productId: productId});
 });
 
-Meteor.publish('productCount', function() {
+Meteor.publish('productCount', function () {
   return Products.find().count();
 });
 
-Meteor.publish('colors', function() {
+Meteor.publish('colors', function () {
   return Colors.find();
 });
 
-Meteor.publish('vendors', function() {
+Meteor.publish('vendors', function () {
   return Vendors.find();
-})
+});
+
+Meteor.publish('productTypes', function () {
+  return ProductTypes.find();
+});
+
+Meteor.publish('seasons', function () {
+  return Seasons.find();
+});
 
 //TODO: implement event privacy
-Meteor.publish('calEvents', function() {
+Meteor.publish('calEvents', function () {
   return CalEvents.find();
 });
 
