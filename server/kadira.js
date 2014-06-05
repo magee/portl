f61fad3bbcff25c0326dd88c7e2c006277172d65
Magee Mooney <mageemooney@me.com>
2014-06-05 15:11:56 -0700

    adds kadira.js setup file and modifies .gitignore to remove it as an exception

--- server/kadira.js
+++ server/kadira.js
@@ -1,4 +1,4 @@
 /**
  * Created by mageemooney on 6/5/14.
  */
-Kadira.connect(process.env.KADIRA_APP_ID, process.env.KADIRA_APP_SECRET);
+//Kadira.connect(process.env.KADIRA_APP_ID, process.env.KADIRA_APP_SECRET);
