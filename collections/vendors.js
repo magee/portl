c5c187325e83802c4f3614ed4d5e30d85df2bb91
Magee Mooney <mageemooney@me.com>
2014-06-07 12:58:21 -0700

    adds refactoring TODO for Vendors/Products

--- collections/vendors.js
+++ collections/vendors.js
@@ -2,3 +2,4 @@
  * Created by mageemooney on 5/18/14.
  */
 Vendors = new Meteor.Collection('vendors');
+//TODO: refactor Vendors collection so fieldnames are consistent with those in products (or vice verse)
