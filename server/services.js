393da000def1d989c1e28f6229f3dac286b3d69f
Magee Mooney <mageemooney@me.com>
2014-06-08 00:03:32 -0700

    linting cleanup

--- server/services.js
+++ server/services.js
@@ -1,16 +1,17 @@
 /**
  * Created by mageemooney on 5/5/14.
  */
+'use strict';
 
 Meteor.methods({
-  findLast : function(collection, filter){
+  findLast : function (collection, filter){
     var criteria = filter || '{}';
     return collection.findOne(criteria, {sort: {createdAt: -1}});
   },
-  getCount : function(collection, filter) {
+  getCount : function (collection, filter) {
     return collection.find(filter).count();
   },
-  getSeasons: function() {
+  getSeasons: function () {
     var results = [];
     var seasons = Seasons.find();
 
@@ -21,7 +22,7 @@ Meteor.methods({
 
     return results;
   },
-  getCategories: function() {
+  getCategories: function () {
 //    return ProductTypes.find() || {};
     return ProductTypes.find();
 
@@ -34,5 +35,6 @@ Meteor.methods({
 
     return results;
   }
+
 });
 
