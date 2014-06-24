4c062c3404fcc95b0df026458ac29d7d3391015d
Magee Mooney <mageemooney@me.com>
2014-06-24 08:27:12 -0700

    removes deprecated template event handler from layout.js

--- client/views/application/layout.js
+++ client/views/application/layout.js
@@ -69,31 +69,3 @@ Template.layout.events({
     $('#myModal').modal({show:true});
   }
 });
-
-Template.tandp_login.events({
-
-  'submit #login-form' : function(e, t){
-    e.preventDefault();
-    // retrieve the input field values
-    var email = t.find('#login-email').value
-      , password = t.find('#login-password').value;
-
-    console.log('running login-form submit event');
-
-    // Trim and validate your fields here....
-
-    // If validation passes, supply the appropriate fields to the
-    // Meteor.loginWithPassword() function.
-    Meteor.loginWithPassword(email, password, function(err){
-      console.log('ran loginWithPassword');
-      if (err)  {
-        // The user might not have been found, or their passwword
-        // could be incorrect. Inform the user that their
-        // login attempt has failed.
-      } else {
-        // The user has been logged in.
-      }
-    });
-    return false;
-  }
-});
