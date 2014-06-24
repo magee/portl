c5594a9f82c4b30d6490530784808c0096c8c69d
Magee Mooney <mageemooney@me.com>
2014-06-24 13:26:34 -0700

    strip unused code, general cleanup

--- client/views/application/layout.js
+++ client/views/application/layout.js
@@ -2,14 +2,6 @@
  * Created by mageemooney on 5/4/14.
  */
 
-$('.thumbnail').click(function(){
-  $('.modal-body').empty();
-  var title = $(this).parent('a').attr("title");
-  $('.modal-title').html(title);
-  $($(this).parents('div').html()).appendTo('.modal-body');
-  $('#myModal').modal({show:true});
-});
-
 Template.layout.helpers({
   pageTitle: function() { return Session.get('pageTitle'); } ,
   modalContent: function() {
@@ -20,52 +12,5 @@ Template.layout.helpers({
 Template.layout.events({
   'click [data-toggle=offcanvas]': function() {
     $('.row-offcanvas').toggleClass('active');
-  },
-  'click #login': function() {
-    $('.modal-body').empty();
-    var title = $(this).parent('a').attr("title");
-    var content = '<div id="login-dropdown-list" class="accounts-dialog">' +
-               '<a class="login-close-text">Close</a>' +
-               '<div class="login-close-text-clear"></div>' +
-               '<div class="login-text-and-button">' +
-               '<div class="login-button single-login-button" id="login-buttons-google">' +
-               '<div class="login-image" id="login-buttons-image-google"></div>' +
-               '<span class="text-besides-image sign-in-text-google">Sign in with Google</span>' +
-               '</div></div>' +
-               '<div class="or">' +
-               '<span class="hline">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>' +
-               '<span class="or-text">or</span>' +
-               '<span class="hline">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>' +
-               '</div>' +
-               '<div class="login-form login-password-form">' +
-               '<div id="login-username-or-email-label-and-input">' +
-               '<label id="login-username-or-email-label" for="login-username-or-email">' +
-               'Username or Email' +
-               '</label>' +
-               '<input id="login-username-or-email" type="text" autocomplete="off" style="background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH3QsPDiIqO1Am6gAAAb1JREFUOMvNkk1rU0EUhp8zd0gCrdKFNgiCFatiIYgUKdW6aikuhIJF6kJcdFFEXAclzM0XJH9A6EL8BSoqZlEQCl0GEQU/qApdiYouKmgUbu7luGgC+boUuvLdvcPMc86c88J/p2w2e9g5d7btnXNTzrlM3H3TaZxzt1Kp1KaI3AcEEBFZFZFXvu9XBgFsjw9EZAjIOOeWVDUUkTMAqvppEEB6ve/7GyJyAfioqpGInALWi8XibCwgl8sdMcbsbzabf621Y8aYNRHxWpUjYFFVv4vIcBiGPyqVyuuuL1hrbwM3kslkf4Ud0BORnWattfeAld4hmr1uTVrTn1TVg6r6U0RGPc97DJh21V0Bncrn88+BOVV9Y4zp2v/w0RkWzo2w8aDG52BwDq4Ccy1b7iInJrh2fZbx8QxjQzFBAk4Aoaq+K5VKDztec3H5MmkAIppxSSyXy6UgCE5HUXQT0Pb58UvLTB34Qm1tE4CwEZ9EqtXq++6TUaYn0xD9YuZ8Gkgwv7LA1t2nbA8C9OsPH16+peGFpA6dZGQfbH/9RiOug379pl57RB1ITCxy58oxXjyrE8StsVOFQkF3w/8DCTuL1wm1OYIAAAAASUVORK5CYII=); background-attachment: scroll; background-position: 100% 50%; background-repeat: no-repeat;">' +
-               '</div>' +
-               '<div id="login-password-label-and-input">' +
-               '<label id="login-password-label" for="login-password">' +
-               'Password' +
-               '</label>' +
-               '<input id="login-password" type="password" autocomplete="off" style="background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH3QsPDiIqO1Am6gAAAb1JREFUOMvNkk1rU0EUhp8zd0gCrdKFNgiCFatiIYgUKdW6aikuhIJF6kJcdFFEXAclzM0XJH9A6EL8BSoqZlEQCl0GEQU/qApdiYouKmgUbu7luGgC+boUuvLdvcPMc86c88J/p2w2e9g5d7btnXNTzrlM3H3TaZxzt1Kp1KaI3AcEEBFZFZFXvu9XBgFsjw9EZAjIOOeWVDUUkTMAqvppEEB6ve/7GyJyAfioqpGInALWi8XibCwgl8sdMcbsbzabf621Y8aYNRHxWpUjYFFVv4vIcBiGPyqVyuuuL1hrbwM3kslkf4Ud0BORnWattfeAld4hmr1uTVrTn1TVg6r6U0RGPc97DJh21V0Bncrn88+BOVV9Y4zp2v/w0RkWzo2w8aDG52BwDq4Ccy1b7iInJrh2fZbx8QxjQzFBAk4Aoaq+K5VKDztec3H5MmkAIppxSSyXy6UgCE5HUXQT0Pb58UvLTB34Qm1tE4CwEZ9EqtXq++6TUaYn0xD9YuZ8Gkgwv7LA1t2nbA8C9OsPH16+peGFpA6dZGQfbH/9RiOug379pl57RB1ITCxy58oxXjyrE8StsVOFQkF3w/8DCTuL1wm1OYIAAAAASUVORK5CYII=); background-attachment: scroll; background-position: 100% 50%; background-repeat: no-repeat;">' +
-               '</div>' +
-               '<div class="login-button login-button-form-submit" id="login-buttons-password">' +
-               'Sign in' +
-               '</div>' +
-               '<div class="additional-link-container">' +
-               '<a id="signup-link" class="additional-link">Create account</a>' +
-               '</div>' +
-               '<div class="additional-link-container">' +
-               '<a id="forgot-password-link" class="additional-link">Forgot password</a>' +
-               '</div>' +
-               '</div>' +
-               '</div>';
-
-
-    $('.modal-title').html(title);
-    $('.modal-body').html(content);
-    /*$($(this).next('div').html()).appendTo('.modal-body');*/
-    $('#myModal').modal({show:true});
   }
 });
