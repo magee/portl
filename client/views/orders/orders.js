639a0228056a5d65701488a858ba4409d3db25ee
Magee Mooney <mageemooney@me.com>
2014-06-24 12:43:14 -0700

    more order page template and event handling, adds Orders collection

--- client/views/orders/orders.js
+++ client/views/orders/orders.js
@@ -1,3 +1,13 @@
 /**
  * Created by mageemooney on 6/24/14.
  */
+Template.orders.helpers({
+  orders: function() {
+    return Orders.find({});
+  },
+  orderCount: function() {
+    return Meteor.call('orderCount');
+  }
+});
+
+Template.orders.events({});
---
+++ collections/orders.js
@@ -0,0 +1,4 @@
+/**
+ * Created by mageemooney on 6/24/14.
+ */
+Orders = new Meteor.Collection('orders');
